77c5c7661a913d3d5f83d4163a1710e9736b7a04
Mardvey-UMA <chikernut213@gmail.com>
2025-04-07 21:55:26 +0400

    fix tg bot

--- tg_bot/handlers/common.py
+++ tg_bot/handlers/common.py
@@ -9,11 +9,17 @@ router = Router(name="common")
 
 @router.message(CommandStart())
 async def handle_start(message: Message, user_service: IUserService):
-    await message.answer("Open", reply_markup=main_markup())
-
-
-    # user = await user_service.register_user(
-    #     telegram_id=message.from_user.id,
-    #     chat_id=message.chat.id,
-    #     username=message.from_user.username or ""
-    # )
+    _ = await user_service.register_user(
+        telegram_id=message.from_user.id,
+        chat_id=message.chat.id,
+        username=message.from_user.username or ""
+    )
+    await message.answer(
+        "🎓 <b>Добро пожаловать в магазин мерча СГУ!</b> 🎓\n\n"
+        "✨ Здесь вы найдете <i>стильные и полезные</i> товары с символикой <b>Саратовского Государственного Университета</b>:\n\n"
+        "📝 <b>Канцелярия:</b> блокноты, ручки, тетради, значки и стикеры\n"
+        "🛒 Выбирайте, что вам по душе, и носите гордую символику своего университета! 💙\n\n"
+        "👇 <i>Что вас интересует?</i>",
+        reply_markup=main_markup(),
+        parse_mode="HTML"
+    )
