405b517a7b6cc1aca4e5a6b27651c44a2ec363fc
Mardvey-UMA <chikernut213@gmail.com>
2025-04-07 11:33:16 +0400

    front add wishlistpage

--- frontend/src/hooks/useProductSearch.ts
+++ frontend/src/hooks/useProductSearch.ts
@@ -1,14 +1,14 @@
-import { useQuery } from '@tanstack/react-query'
-import { ProductService } from '../api/product'
+// import { useQuery } from '@tanstack/react-query'
+// import { ProductService } from '../api/product'
 
-export const useProductSearch = (
-	categoryId: number,
-	filters: Record<number, string[]>,
-	page: number = 1
-) => {
-	return useQuery({
-		queryKey: ['products', 'search', categoryId, filters, page],
-		queryFn: () => ProductService.search(categoryId, filters, page),
-		staleTime: 1000 * 60 * 5,
-	})
-}
+// export const useProductSearch = (
+// 	categoryId: number,
+// 	filters: Record<number, string[]>,
+// 	page: number = 1
+// ) => {
+// 	return useQuery({
+// 		queryKey: ['products', 'search', categoryId, filters, page],
+// 		queryFn: () => ProductService.search(categoryId, filters, page),
+// 		staleTime: 1000 * 60 * 5,
+// 	})
+// }
--- frontend/src/pages/WishListPage/WishlistPage.tsx
+++ frontend/src/pages/WishListPage/WishlistPage.tsx
@@ -1,9 +1,62 @@
+import { Button, Spin, Typography } from 'antd'
+import { useState } from 'react'
+import { WishListItem } from '../../components/WishListItem/WishListItemComponent'
+import { useWishlist } from '../../hooks/useWishList'
 import styles from './WishlistPage.module.scss'
 
+const { Title } = Typography
+
 export const WishlistPage = () => {
+	const [page, setPage] = useState(0)
+	const { data, isLoading, isFetching } = useWishlist(page)
+	const [removedArticles, setRemovedArticles] = useState<number[]>([])
+
+	const handleLoadMore = () => setPage(prev => prev + 1)
+	const handleRemove = (article: number) =>
+		setRemovedArticles(prev => [...prev, article])
+
+	const filteredItems =
+		data?.items.filter(
+			item => !removedArticles.includes(item.article_number)
+		) || []
+
 	return (
 		<div className={styles.container}>
-			<h1>список желаний</h1>
+			<Title level={2} className={styles.title}>
+				Список желаний
+			</Title>
+
+			{isLoading ? (
+				<Spin size='large' className={styles.spinner} />
+			) : (
+				<>
+					<div className={styles.list}>
+						{filteredItems.map(item => (
+							<WishListItem
+								key={item.article_number}
+								article={item.article_number}
+								onRemove={handleRemove}
+							/>
+						))}
+					</div>
+
+					{data?.current_page < data?.total_pages && (
+						<div className={styles.loadMore}>
+							<Button
+								type='primary'
+								loading={isFetching}
+								onClick={handleLoadMore}
+							>
+								Показать еще
+							</Button>
+						</div>
+					)}
+				</>
+			)}
+
+			{!isLoading && filteredItems.length === 0 && (
+				<div className={styles.empty}>В списке желаний пока нет товаров</div>
+			)}
 		</div>
 	)
 }
