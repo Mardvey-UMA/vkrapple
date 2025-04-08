af5e5dff42575b052ba80e50c50abdbd7bd707de
Mardvey-UMA <chikernut213@gmail.com>
2025-04-08 22:59:17 +0400

    fix bug

--- backend/src/main/kotlin/ru/webshop/backend/repository/UserRepository.kt
+++ backend/src/main/kotlin/ru/webshop/backend/repository/UserRepository.kt
@@ -3,11 +3,27 @@ package ru.webshop.backend.repository
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import ru.webshop.backend.entity.User
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface UserRepository: JpaRepository<User, Long> {
-    @EntityGraph(attributePaths = ["roles"])
-    fun findByTelegramId(telegramId: Long): User?
+    //@EntityGraph(attributePaths = ["roles"])
+    //fun findByTelegramId(telegramId: Long): User?
 
-    @EntityGraph(attributePaths = ["roles"])
-    fun existsByTelegramId(telegramId: Long): Boolean
+    //@EntityGraph(attributePaths = ["roles"])
+    //fun existsByTelegramId(telegramId: Long): Boolean
+
+    @Query("""
+        SELECT u FROM User u 
+        LEFT JOIN FETCH u.roles 
+        WHERE u.telegramId = :telegramId
+    """)
+    fun findByTelegramId(@Param("telegramId") telegramId: Long): User?
+
+    @Query("""
+        SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END 
+        FROM User u 
+        WHERE u.telegramId = :telegramId
+    """)
+    fun existsByTelegramId(@Param("telegramId") telegramId: Long): Boolean
 }
