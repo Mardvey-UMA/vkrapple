fb63947450f89eb1b380cf012bf81bd00e1d521e
Mardvey-UMA <chikernut213@gmail.com>
2025-05-03 14:13:09 +0400

    add search query elastic

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,23 @@
-//import tailwindcss from '@tailwindcss/vite'
+// //import tailwindcss from '@tailwindcss/vite'
+// import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+
+// export default defineConfig({
+// 	plugins: [react()],
+// 	server: {
+// 		host: true,
+// 		allowedHosts: ['www.ssushop.ru', 'ssushop.ru']
+// 	},
+// 	preview: {
+// 	    host: true,
+// 	    port: 3000,
+// 	    allowedHosts: [
+// 	      "www.ssushop.ru",
+// 	      "ssushop.ru",
+// 	      "localhost"
+// 	    ]
+// 	  }
+// })
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
@@ -6,15 +25,21 @@ export default defineConfig({
 	plugins: [react()],
 	server: {
 		host: true,
-		allowedHosts: ['www.ssushop.ru', 'ssushop.ru']
+		proxy: {
+			'/api': {
+				target: 'https://backend.localhost',
+				changeOrigin: true,
+			},
+		},
 	},
 	preview: {
-	    host: true,
-	    port: 3000,
-	    allowedHosts: [
-	      "www.ssushop.ru",
-	      "ssushop.ru",
-	      "localhost"
-	    ]
-	  }
+		host: true,
+		port: 5173,
+		allowedHosts: [
+			'www.ssushop.ru',
+			'ssushop.ru',
+			'localhost',
+			'backend.localhost',
+		],
+	},
 })
